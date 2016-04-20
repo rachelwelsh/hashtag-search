69dd6ebaca60a71477dfaa49f57c0bf6b1294022
rachelwelsh <rachh.w@live.com>
2016-04-20 10:35:37 +0100

    app

---
+++ search.js
@@ -0,0 +1,74 @@
+window.Instagram = {
+    /**
+     * Store application settings
+     */
+    config: {},
+
+    BASE_URL: 'https://api.instagram.com/v1',
+
+    init: function( opt ) {
+        opt = opt || {};
+
+        this.config.client_id = opt.client_id;
+    },
+
+    /**
+     * Get a list of popular media.
+     */
+    popular: function( callback ) {
+        var endpoint = this.BASE_URL + '/media/popular?client_id=' + this.config.client_id;
+        this.getJSON( endpoint, callback );
+    },
+
+    /**
+     * Get a list of recently tagged media.
+     */
+    tagsByName: function( name, callback ) {
+        var endpoint = this.BASE_URL + '/tags/' + name + '/media/recent?client_id=' + this.config.client_id;
+        this.getJSON( endpoint, callback );
+    },
+
+    getJSON: function( url, callback ) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'jsonp',
+            success: function( response ) {
+                if ( typeof callback === 'function' ) callback( response );
+            }
+        });
+    }
+};
+
+Instagram.init({
+    client_id: '6adedf760d08471099116d3eb04ee026'
+});
+
+
+$( document ).ready(function() {
+
+    Instagram.popular(function( response ) {
+        var $instagram = $( '#instagram' );
+        for ( var i = 0; i < response.data.length; i++ ) {
+            imageUrl = response.data[i].images.low_resolution.url;
+            $instagram.append( '<img src="' + imageUrl + '" />' );
+        }
+    });
+
+    $( '#form' ).on('submit', function( e ) {
+        e.preventDefault();
+
+        var tagName = $( '#search' ).val();
+        Instagram.tagsByName(tagName, function( response ) {
+            var $instagram = $( '#instagram' );
+            $instagram.html('');
+
+            for ( var i = 0; i < response.data.length; i++ ) {
+                imageUrl = response.data[i].images.low_resolution.url;
+                $instagram.append( '<img src="' + imageUrl + '" />' );
+            }
+        });
+
+    });
+
+});
